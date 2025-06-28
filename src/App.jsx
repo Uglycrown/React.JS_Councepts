d79abcc22b76cd1315c0226cbd7675b28ff9a90c
Uglycrown <gourav.singh9756@gmail.com>
2025-06-28 12:12:12 +0530

    Input tag text work in REactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-
-import { useState } from 'react'
-
-import User from './user';
-import College from './College';
-import Student from './Student';
-
+import { useState } from "react";
 function App() {
- const [display, setDisplay ] = useState(true);
-   const [count, setCount] = useState(0)
-   let userObject = {
-    name: "John",
-    age: 30,
-    location: "New York"
-   };   
-   let CollegeNames = ['IET', 'IIIT', 'NIT', 'IIT', 'BIT'];
-  const [student, setStudent] = useState("rahul")
-    return (
-        <>
-        <h1>Props in react js</h1>
-        {/* <College names={CollegeNames} /> */}
-         {student && <Student name={student} />}
-          <button onClick={() => setStudent("new name")} >update Student Name</button>
-         <User userObject={userObject} name="student" />
-        </>
-    )
+  const [value, setValue] = useState('Rahul')
+  return (
+    <div>
+      <h1>Hello World</h1>
+     
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder="Type something..." />
+      <p>You typed: {value}</p>
+      <button onClick={() => setValue('')}>Clear Value</button>
+      <h1>{value}</h1>
+    </div>
+  );
 }
+
 
 export default App;
--- src/Student.jsx
+++
@@ -1,9 +0,0 @@
-function Student({ name }) {
-    return (
-        <>
-          <h1>Name: {name}</h1>
-        </>
-    );
-}
-
-export default Student;
